9c5390a3ff396af63431e7af691a3d1a037c7be8
Konstantin Reus <konstantin.reus@cloud.ionos.com>
2021-03-27 15:25:01 +0100

    Merge Sorted Arrays

--- src/com/reus/leetcode/MergeSortedArrays.java
+++ src/com/reus/leetcode/MergeSortedArrays.java
@@ -1,33 +1,32 @@
 package com.reus.leetcode;
 
-import java.util.Arrays;
-
+/**
+ * https://leetcode.com/problems/merge-sorted-array
+ */
 public class MergeSortedArrays {
-    public static void main(String[] args) {
-        MergeSortedArrays m = new MergeSortedArrays();
-        int[] l = new int[]{1, 2, 3, 0, 0, 0};
-        int[] r = new int[]{2, 5, 6};
-        m.merge(l, 6, r, 3);
-        System.out.println(Arrays.toString(l));
-    }
-
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int r = 0;
-        int l = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            while (l < nums1.length && nums1[l] < nums2[r]) {
-                l++;
-            }
-            shiftRight(nums1, l);
-            nums1[l] = nums2[r++];
+        int[] arr = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr[i] = nums1[i];
         }
-    }
-
-    private void shiftRight(int[] arr, int start) {
-        for (int i = arr.length - 1; i > start; i--) {
-            arr[i] = arr[i - 1];
+        int writeIndex = nums1.length - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        for (int i = 0; i < m + n; i++) {
+            Integer toAdd = null;
+            if (p1 >= 0 && p2 >= 0) {
+                toAdd = Math.max(nums2[p2], nums1[p1]);
+                if (nums2[p2] > nums1[p1]) {
+                    p2--;
+                } else {
+                    p1--;
+                }
+            } else if (p1 >= 0 && p2 < 0) {
+                toAdd = nums1[p1--];
+            } else if (p2 >= 0 && p1 < 0) {
+                toAdd = nums2[p2--];
+            }
+            nums1[writeIndex--] = toAdd;
         }
     }
 }
