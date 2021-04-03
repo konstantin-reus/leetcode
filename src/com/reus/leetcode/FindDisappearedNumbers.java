8943579a0ab6ea1ca1a1760e8e5793b3e0c87fea
Konstantin Reus <konstantin.reus@cloud.ionos.com>
2021-04-03 19:18:23 +0200

    Find disappeared numbers

---
+++ src/com/reus/leetcode/FindDisappearedNumbers.java
@@ -0,0 +1,23 @@
+package com.reus.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDisappearedNumbers {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] *= -1;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+
+    }
+}
