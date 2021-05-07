9a966b319c5362dbc93c382a93c6a6f68ef792b7
Konstantin Reus <konstantin.reus@cloud.ionos.com>
2021-05-08 00:30:26 +0200

    Minimum Path Sum

--- src/com/reus/leetcode/MinimumPathSum.java
+++ src/com/reus/leetcode/MinimumPathSum.java
@@ -1,6 +1,9 @@
 package com.reus.leetcode;
 
 /**
+ *
+ * https://leetcode.com/problems/minimum-path-sum/
+ *
  * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom
  * right which minimizes the sum of all numbers along its path.
  * <p>
@@ -19,22 +22,32 @@ package com.reus.leetcode;
  */
 public class MinimumPathSum {
     public int minPathSum(int[][] grid) {
-        int[][] sums = new int[grid.length][grid[0].length];
-        sums[sums.length - 1][sums[0].length - 1] = grid[grid.length - 1][grid[0].length - 1];
-
-        for (int y = sums.length - 1; y >= 0; y--) {
-            for (int x = sums[y].length - 1; x >= 0; x--) {
-                if (y == sums.length - 1 && x == sums[y].length - 1) {
-                    continue;
-                } else if (y == sums.length - 1) {
-                    sums[y][x] = sums[y][x + 1] + grid[y][x];
-                } else if (x == sums[y].length - 1) {
-                    sums[y][x] = sums[y + 1][x] + grid[y][x];
-                } else {
-                    sums[y][x] = grid[y][x] + Math.min(sums[y + 1][x], sums[y][x + 1]);
+        if (grid.length == 0) {
+            return 0;
+        }
+        int[][] aux = new int[grid.length][grid[0].length];
+        for (int y = 0; y < aux.length; y++) {
+            for (int x = 0; x < aux[y].length; x++) {
+                aux[y][x] = Integer.MAX_VALUE;
+            }
+        }
+        aux[0][0] = grid[0][0];
+        for (int y = 0; y < aux.length; y++) {
+            for (int x = 0; x < aux[y].length; x++) {
+                if (x + 1 < aux[y].length) {
+                    int newSum = aux[y][x] + grid[y][x+1];
+                    if (aux[y][x+1] > newSum) {
+                        aux[y][x+1] = newSum;
+                    }
+                }
+                if (y + 1 < aux.length) {
+                    int newSum = aux[y][x] + grid[y+1][x];
+                    if (aux[y+1][x] > newSum) {
+                        aux[y+1][x] = newSum;
+                    }
                 }
             }
         }
-        return sums[0][0];
+        return aux[aux.length -1][aux[aux.length -1].length -1];
     }
 }
