98a94f241645fd026fc72d096534c05487245757
Konstantin Reus <kr5031@gmail.com>
2024-12-01 15:36:57 +0100

    [screening] 02 Odd Even Jump (accepted)

--- src/com/reus/leetcode/google/screening/OddEvenJumps.kt
+++ src/com/reus/leetcode/google/screening/OddEvenJumps.kt
@@ -1,46 +1,36 @@
 package com.reus.leetcode.google.screening
 
+import java.util.TreeMap
+
+/**
+ * https://leetcode.com/problems/odd-even-jump/
+ */
 class OddEvenJumps {
     fun oddEvenJumps(arr: IntArray): Int {
         val reachabilityOdd = BooleanArray(arr.size) { false }
         val reachabilityEven = BooleanArray(arr.size) { false }
         reachabilityOdd[reachabilityOdd.size - 1] = true
         reachabilityEven[reachabilityEven.size - 1] = true
+        val valueToIndex = TreeMap<Int, Int>()
+        valueToIndex[arr[arr.size - 1]] = arr.size - 1
+
         for (i in arr.size - 2 downTo 0) {
-            val nextOddJumpIndex = findNextOddJumpIndex(arr, i)
-            val nextEvenJumpIndex = findNextEvenJumpIndex(arr, i)
+            val nextOddJumpIndex = findNextOddJumpIndex(arr[i], valueToIndex)
+            val nextEvenJumpIndex = findNextEvenJumpIndex(arr[i], valueToIndex)
             val isOddJumpPossible = nextOddJumpIndex != null && reachabilityEven[nextOddJumpIndex]
             val isEvenJumpPossible = nextEvenJumpIndex != null && reachabilityOdd[nextEvenJumpIndex]
             reachabilityOdd[i] = isOddJumpPossible
             reachabilityEven[i] = isEvenJumpPossible
+            valueToIndex[arr[i]] = i
         }
-        return reachabilityOdd.toList().filter {it}.count()
+        return reachabilityOdd.toList().filter { it }.count()
     }
 
-    private fun findNextOddJumpIndex(arr: IntArray, startFrom: Int): Int? {
-        val base = arr[startFrom]
-        var result: Int? = null;
-        for (i in startFrom + 1..arr.size - 1) {
-            if (arr[i] >= base && (result == null || arr[i] < arr[result])) {
-                result = i
-            }
-        }
-        return result
+    private fun findNextOddJumpIndex(cur: Int, valueToIndex: TreeMap<Int, Int>): Int? {
+        return valueToIndex.ceilingEntry(cur)?.let { (_, index) -> index }
     }
 
-    private fun findNextEvenJumpIndex(arr: IntArray, startFrom: Int): Int? {
-        val base = arr[startFrom]
-        var result: Int? = null;
-        for (i in startFrom + 1..arr.size - 1) {
-            if (arr[i] <= base && (result == null || arr[i] > arr[result])) {
-                result = i
-            }
-        }
-        return result
+    private fun findNextEvenJumpIndex(cur: Int, valueToIndex: TreeMap<Int, Int>): Int? {
+        return valueToIndex.floorEntry(cur)?.let { (_, index) -> index }
     }
-}
-
-fun main() {
-    val result = OddEvenJumps().oddEvenJumps(intArrayOf(81,54,96,60,58))
-    println(result)
 }
